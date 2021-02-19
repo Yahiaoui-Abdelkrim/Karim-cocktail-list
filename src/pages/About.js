90553b7ac475d9bdbdc23f270e53cc37fd8e39f4
karim <karim.yah2024@gmail.com>
2021-02-19 03:19:54 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Aliquam inventore totam quae obcaecati adipisci placeat a
+        soluta! Autem, neque assumenda.
+      </p>
+    </section>
+  );
+};
+
+export default About;
